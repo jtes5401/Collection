94c4a3a42054a893ccb3b5d08ba033730f6bd3d5
Wei Kuo <jtes5401@gmail.com>
2020-03-09 21:14:26 +0800

    Initial Commit

---
+++ Collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection
+//
+//  Created by Wei Kuo on 2020/3/9.
+//  Copyright © 2020 Wei Kuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
