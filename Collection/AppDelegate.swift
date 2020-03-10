4ae4be38281da7efb247675a5872d2fcf3228e93
Wei Kuo <jtes5401@gmail.com>
2020-03-10 22:07:30 +0800

    Change import SDWebImage and webP decoder

--- Collection/AppDelegate.swift
+++ Collection/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import SDWebImageWebPCoder
+import SDWebImage
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        SDImageCodersManager.shared.addCoder(SDImageWebPCoder.shared)
         return true
     }
 
